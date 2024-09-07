eb32024ab86014fda8cf1706a323a9f07c85dc14
moshood-wale <moshood815@gmail.com>
2024-09-07 17:41:04 +0100

    Created models for User, Organisation, Job, and Application tables

--- hr/admin.py
+++ hr/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Organisation, OrganisationStaff, Application, Job
 
-# Register your models here.
+
+admin.site.register(User)
+admin.site.register(Organisation)
+admin.site.register(OrganisationStaff)
+admin.site.register(Application)
+admin.site.register(Job)
